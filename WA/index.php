bca81bb89fc01b0541d711674b99a2b630f669ab
JJZabransky <113086055+JJZabransky@users.noreply.github.com>
2023-06-04 19:14:23 +0200

    Add files via upload

---
+++ WA/index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '':
+    case '/':
+        $redirect = '/views/index.php';
+        break;
+    
+    case '/about':
+        $redirect = '/views/aboutus.php';
+        break;
+    
+    case '/download':
+        $redirect = '/views/download.php';
+        break;
+    
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        exit();
+}
+
+$_SESSION['site'] = $redirect;
+require_once __DIR__ . '/views/core/header.php';
+require_once __DIR__ . $redirect ?? __DIR__ . '/views/index.php';
+require_once __DIR__ . '/views/core/footer.php';
